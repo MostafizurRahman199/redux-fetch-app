ef96d9ddf3ecce342e905c8d5116a88f0ef4e8bd
MostafizurRahman199 <67595254+MostafizurRahman199@users.noreply.github.com>
2023-02-12 11:39:56 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Todos from './Components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>This is Todos App</h2>
+      <Todos></Todos>
+    </div>
+  );
+}
+
+export default App;
